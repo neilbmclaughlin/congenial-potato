870337a0121ef160aa161f6d5e91827601bbc04b
neil mclaughlin <neil.mclaughlin@digital.nhs.uk>
2016-11-30 22:17:23 +0000

    small change

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var port = process.env.port || 3000;
 
 app.get('/', function (req, res) {
   console.log({ headers: req.headers});
-  res.send('Hello World!')
+  res.send('Hi World!')
 })
 
 app.listen(port, function () {
